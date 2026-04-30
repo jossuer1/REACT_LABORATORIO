96063c5aaacb931124e91e94218013e744627ac9
Emily <emily.pazmino02@epn.edu.ec>
2026-04-30 12:18:17 -0500

    Cambios en Zustand

---
+++ Zustand_hijo.jsx
@@ -0,0 +1,26 @@
+import storeMusica from "../context/storeMusica"
+
+const Octavo = () => {
+
+  const {detalle} = storeMusica()
+
+  return (
+    <>
+      <h2 className="text-purple-700">Hijo</h2>
+
+      <div className="flex justify-center mb-8">
+
+        <div className="max-w-sm border-2 border-purple-500 rounded-lg p-4 w-80 text-center">
+
+          <p>Canción: {detalle.nombre}</p>
+
+          <p>Género: {detalle.genero}</p>
+
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default Octavo
